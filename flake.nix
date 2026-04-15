bd722a2fc925a7594f931f0a913f84c830b9c3fa
shygyver <shygyver@nixos>
2026-04-15 21:29:48 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A development environment for Bun projects";
+
+  inputs = {
+    # Using unstable for the latest Bun versions
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bun
+          ];
+
+          shellHook = ''
+            echo "🌿 Bun development environment loaded!"
+            bun --version
+          '';
+        };
+      }
+    );
+}
+
